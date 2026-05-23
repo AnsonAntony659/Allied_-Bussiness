c95433d2b6f1b7a205d1b9ffcfa0c6e7b4876060
Anson <ansonantony6002@gmail.com>
2026-05-24 00:43:48 +0530

    added models for all

---
+++ Project/themes/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# model for theme
+
+class SiteSetting(models.Model):
+    banner=models.ImageField(upload_to='media/site/')
+    caption=models.TextField()
+
+
+    
